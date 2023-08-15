002ce86a6d3d1f218a9b821c4753adc66b967815
bintadam <zakiyaadan12@gmail.com>
2023-08-15 20:04:34 +0200

    added h1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ExpenseList from "./components/ExpenseList";
 function App() {
   return (
     <div className="">
+      <h1 className="text-center font-bold text-2xl text-blue-800 pt-20">TRACK YOUR EXPENSE</h1>
       <AddExpense/>
       <ExpenseList/>
     </div>
--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -6,23 +6,36 @@ function AddEXpense(){
     const [amount, setAmount] = useState('')
     const [description, setDescription] = useState('')
 
+    const getToday = () => {
+        const today = new Date();
+        const dd = String(today.getDate()).padStart(2,0)
+        const mm = String(today.getMonth() + 1).padStart(2, '0'); 
+        const yyyy = today.getFullYear();
+        return yyyy + '-' + mm + '-' + dd;
+
+    }
+
     const handleClick = (e) => {
         e.preventDefault()
-        if(!amount) return;
-        if(!description) return;
-        if(!date){
-            return 
+        if(!amount && !description){
+            return "fields cannot be empty"
+        }else if(!amount){
+            return "Amount field cannot be empty" 
+        } else if(!description) {
+            return "Description field cannot be empty"
+        }else if(!date){
+            setDate(getToday());
         }
     }
 
     return (
-        <div className="pt-24 px-80">
-            <form className="grid grid-cols-2 w-full">
-                <input type="text" placeholder="DESCRIPTION" className="w-full my-2 focus:outline-none"/>
-                <input type="number" placeholder="$0" className="border-0 focus:outline-none"/>
-                <input type="date" value={date} onChange={(e) => setDate(e.target.value)} placeholder="date" className="w-full my-2 focus:outline-none"/>
+        <div className="pt-4 px-80">
+            <form className="grid grid-cols-2 w-full text-black gap-5">
+                <input type="text" value={description}placeholder="DESCRIPTION" className="w-full border border-sky-300 p-3 focus:outline-none" onChange={(e) => setDescription(e.target.value)}/>
+                <input type="number" value={amount} placeholder="$0" className="border border-sky-300 p-2 focus:outline-none" onChange={(e) => setAmount(e.target.value)}/>
+                <input type="date" value={date} onChange={(e) => setDate(e.target.value)} placeholder="date" className="w-full my-3 border border-sky-300 p-2 focus:outline-none"/>
             </form>
-            <button className="bg-fuchsia-400 p-3 w-full my-2 rounded-lg text-white text-lg font-bold">ADD EXPENSE
+            <button className="bg-blue-400 p-3 w-full my-2 rounded-lg text-white text-lg font-bold" onClick={handleClick}>ADD EXPENSE
             </button>
         </div>
     )
