6377f6dd633c0bdae8e79b16ba7cbf45241d3a6d
bintadam <zakiyaadan12@gmail.com>
2023-08-15 18:05:38 +0200

    created form

--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -1,7 +1,13 @@
 function AddEXpense(){
     return (
         <div>
-
+            <form>
+                <input type="number" placeholder="$0"/>
+                <input type="text" placeholder="Category"/>
+                <input type="text" placeholder="Description"/>
+                <input type="date" placeholder="date"/>
+                <button>ADD EXPENSE</button>
+            </form>
         </div>
     )
 }
