db19afe44e2fad1e53ff3febe572e164bc00e2e7
bintadam <zakiyaadan12@gmail.com>
2023-08-15 20:20:27 +0200

    created expenses variable

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import AddExpense from "./components/AddExpense";
 import ExpenseList from "./components/ExpenseList";
 
+const expenses =[
+  {id:1, Amount:145, date:04.sep.2021},
+  {id:3, Amount:215, date:11.may.2022},
+  {id:7, Amount:98, date:21.feb.2022},
+  {id:2, Amount:105, date:08.july.2022},
+  {id:8, Amount:125, date:16.nov.2022},
+]
+
 function App() {
   return (
     <div className="">
