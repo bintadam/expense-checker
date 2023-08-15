47df9e2358ef06ac7456a7f62d35f4f901a7558a
bintadam <zakiyaadan12@gmail.com>
2023-08-15 21:23:29 +0200

    created function groupexpensebyyear

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,4 +1,17 @@
 function Expense({expense}){
+
+    function groupExpensesByYear(){
+        return expense.reduce((acc, expense) => {
+            const year = new Date(expense.date).getFullYear();
+            if(!acc[year]){
+                acc[year] =[]
+            }
+            acc[year].push(expense);
+            return acc
+
+        }, {});
+    }
+
     return (
         <li>
             <p>{expense.description}</p>
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,13 +1,20 @@
 import Expense from "./Expense";
 
 function ExpenseList({expenses}){
+    const groupedExpenses = groupExpensesByYear()
     return (
         <div>
-            <ul>
-                {expenses.map((expense) => (
-                    <Expense key={expense.id} expense={expense}/>
-                ))}
-            </ul>
+            <h2 className="text-center font-bold text-xl">Expense List</h2>
+            {Object.entries(groupedExpenses).sort((a, b) => b[0] - a[0]).map(([year, expensesForYear]) => (
+                <div key={year} className="mt-4">
+                    <h3 className="text-center font-semibold text-lg">{year}</h3>
+                    <ul>
+                        {expensesForYear.map((expense) => (
+                            <Expense key={expense.id} expense={expense}/>
+                            ))}
+                    </ul>
+                </div>
+            ))}
         </div>
     )
 };
