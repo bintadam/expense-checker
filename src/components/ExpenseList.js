9ef0875672d953bef1fd64a9d5df2a918706a294
bintadam <zakiyaadan12@gmail.com>
2023-08-15 20:12:07 +0200

    created expense file

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,7 +1,13 @@
-function ExpenseList(){
+import Expense from "./Expense";
+
+function ExpenseList({expenses}){
     return (
         <div>
-
+            <ul>
+                {expenses.map((expense) => (
+                    <Expense key={expense.id} expense={expense}/>
+                ))}
+            </ul>
         </div>
     )
 };
