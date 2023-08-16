a4b910fcf28bdd1f8932f0a136681f3aac0ceb56
bintadam <zakiyaadan12@gmail.com>
2023-08-16 20:49:43 +0200

    installed chart.js

--- src/components/ExpenseChart.js
+++ src/components/ExpenseChart.js
@@ -1,29 +1,29 @@
-function ExpenseChart({initialExpenses =[]}){
-
-    const expenseData =initialExpenses.map((expense) =>{
-        const month = new Date(expense.date).toLocaleString('default', {month:'short'})
-        return {
-            month: month,
-            amount: expense.amount
+function ExpenseChart({expenses}){
+    
+    const monthlyTotals = expenses.reduce((acc, expense) => {
+        const monthYear = new Date(expense.date).toISOString().slice(0, 7);
+        if (!acc[monthYear]) {
+          acc[monthYear] = 0;
         }
-    })
+        acc[monthYear] += expense.Amount;
+        return acc;
+      }, {});
 
-    const highestExpense = Math.max(...expenseData.map(expense => expense.amount))
+    const labels = Object.keys(monthlyTotals);
+    const data = Object.values(monthlyTotals);
+    
+    const chartData = {
+        labels: labels,
+        datasets: [{
+        label: 'Monthly Expenses',
+        data: data,
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1,
+        }]
+    };
 
-    const scaleFactor = 400/highestExpense
-    return (
-        <div>
-            <h6>Expense Chart</h6>
-            <div>
-                {expenseData.map((expense, index) =>(
-                    <div key={index} className="bar" style={{height:`${expense.amount * scaleFactor}px`}}>
-                        <div className="bar-horizontal">{expense.month}</div>
-                        <div className="bar-vertical">{expense.amount}</div>
-                    </div>
-                ))}
-            </div>
-        </div>
-    )
+    return <Bar data={chartData}/>
 }
 
 export default ExpenseChart;
